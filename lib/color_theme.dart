970a0bbc848a42000f99c166beec7b83f1d02a00
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-10-14 23:36:23 +0200

    first commit

--- lib/color_theme.dart
+++ lib/color_theme.dart
@@ -1,7 +1,5 @@
 //TODO: don't forget to add extension of this class to handle HexColor
-import 'package:flutter/material.dart';
 
 class ColorManger {
-  static String farah = "NO ERROR PLEASE";
-  static String name = "ahmed";
+  static String firstCommit = "First commit";
 }
