bedfe187e79bee6a6456485183d1f254a82291da
farah <farahtawfik14@gmail.com>
2022-10-14 23:45:51 +0200

    Revert "second commit"
    This reverts commit 056dc2ef770a65e99798e3d25442cc0130b46323.

--- lib/color_theme.dart
+++ lib/color_theme.dart
@@ -1,5 +1,5 @@
 //TODO: don't forget to add extension of this class to handle HexColor
 
 class ColorManger {
-  static String secondCommit = "second commit";
+  static String firstCommit = "First commit";
 }
