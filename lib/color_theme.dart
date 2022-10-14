056dc2ef770a65e99798e3d25442cc0130b46323
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-10-14 23:39:24 +0200

    second commit

--- lib/color_theme.dart
+++ lib/color_theme.dart
@@ -1,5 +1,5 @@
 //TODO: don't forget to add extension of this class to handle HexColor
 
 class ColorManger {
-  static String firstCommit = "First commit";
+  static String secondCommit = "second commit";
 }
