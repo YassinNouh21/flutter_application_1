b2ac71b10d11de09851a85dfccb6bf4cae36036d
ahmed hany <ahmedhany20200050@gmail.com>
2022-10-14 23:52:54 +0200

    Revert "third commit"
    This reverts commit f4cb833b2e85b9707f04a56000f1f391fdb02d96.

--- lib/color_theme.dart
+++ lib/color_theme.dart
@@ -1,5 +1,5 @@
 //TODO: don't forget to add extension of this class to handle HexColor
 
 class ColorManger {
-  static String first = "first commit";
+  static String secondCommit = "second commit";
 }
